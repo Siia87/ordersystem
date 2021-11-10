587c74c1416bb5ee3fc25a522dcf22d76b7a5f61
Sofia Premuzaj <sofia.premuzaj@gmail.com>
2021-11-10 10:37:39 +0100

    added a comment

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use('/orders', Order)
 
 //const dbconn = process.env.DBCONN || '<defult>'
 
+//connect to db
 const dbconn = 'mongodb + srv://order:order123@cluster0.q5oir.mongodb.net/ordersystem?retryWrites=true&w=majority'
 mongoose.connect(dbconn, () => console.log('connected to db maybe'))
 
